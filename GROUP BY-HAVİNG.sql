7b0e56d0a8037f5fc4a1efad0b0ca65ef3fc041d
Recep Darıcı <recepdarici4507@gmail.com>
2022-08-31 18:29:45 +0300

    sql

---
+++ GROUP BY-HAVİNG.sql
@@ -0,0 +1,6 @@
+/*
+-- GROUP BY -- HAVİNG --
+HAVING ifadesinin işlevi WHERE ifadesininkine çok benziyor. Ancak kümeleme fonksiyonları ile
+WHERE ifadesi birlikte kullanılmadığında HAVING ifadesine ihtiyac duyulmuştur
+GROUP BY ile kullanılır gruplamadan sonraki şart için group by dan sonra HAVING kullanılır
+--Maaş ortalaması 3000’den fazla olan ülkelerdeki erkek çalışanların maaş ortalaması.
